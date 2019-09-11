f9cbe20c274fa4abaeb68cade31421177d4651a6
vivevincere <46767637+vivevincere@users.noreply.github.com>
2019-09-10 23:43:32 -0800

    move setup.sql

--- setup.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE IF NOT EXISTS AuthServer;
-
-USE AuthServer;
-
-CREATE TABLE login_details(
-	Username varchar(100) NOT NULL,
-	PRIMARY KEY(Username),
-	Password binary(100) NOT NULL
-);
