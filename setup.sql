a50c077bbd01cfda257a633582fe783c36118ad3
Vinaceous <s.vincent@live.com.sg>
2019-09-10 16:25:47 -0800

    sql fix and server start

--- setup.sql
+++ setup.sql
@@ -2,7 +2,7 @@ CREATE DATABASE IF NOT EXISTS AuthServer;
 
 USE AuthServer;
 
-CREATE TABLE login_details)
+CREATE TABLE login_details(
 	Username varchar(100) NOT NULL,
 	PRIMARY KEY(Username),
 	Password binary(100) NOT NULL
